796aeb1bd20223074f16eca6b1a740161c12bc9a
myamann <mehmetyaman331@gmail.com>
2021-02-26 13:19:43 +0300

    lottie-starwars added

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,28 @@
+import React, {useEffect,useRef} from 'react'
+import lottie from 'lottie-web';
+
 export default function Home() {
+  const container = useRef(null)
+    
+    useEffect(() =>{
+        lottie.loadAnimation({
+            container : container.current,
+            renderer:'svg',
+            loop:true,
+            autoplay:true,
+            animationData:require('../starwars.json')
+
+        })
+    },[])
+  
   return (
     <div>
       <h1 className="text-2xl text-center my-6 font-semibold">Welcome to Next.js StarWars Wiki</h1>
       <p className="text-center">You can find some information about from StarWars Universe</p>
+      <div className="flex justify-center mt-10">
+            <div className="container  w-3/4 max-w-md h-auto" ref={container}></div>
+        </div>
+     
     </div>
   );
 }
