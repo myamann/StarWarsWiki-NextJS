0bcb909cb98635fbc2eb6bab28f376b3ca7f4a0a
myamann <mehmetyaman331@gmail.com>
2021-02-26 15:12:00 +0300

    head and title added

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,23 @@
-import React from 'react'
-import Nav from './Nav';
-import Footer from './Footer';
+import React from "react";
+import Nav from "./Nav";
+import Footer from "./Footer";
+import Head from 'next/head';
 
-export default function Layout({children}) {
-    return(
-        <div>
-            <Nav/>
-            {children}
-            <Footer/>
-        </div>
-    )
+export default function Layout({ children }) {
+  return (
+    <div>
+      <Head>
+        <meta charSet="utf-8" />
+        <title>StarWars Wiki</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+      </Head>
+      <Nav />
+      {children}
+      <Footer />
+    </div>
+  );
 }
